4252dbc14f7a1000ea59c30730757802f1e4abc5
shubhanggg12 <shubhangprajapati19@gnu.ac.in>
2022-10-12 09:48:14 +0530

    fourth commit

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,12 +1,17 @@
+from email.policy import default
 from turtle import title
 from unittest.util import _MAX_LENGTH
+from wsgiref import validate
 from django.db import models
+from django.core.validators import MinValueValidator , MaxValueValidator
 
 # Create your models here.
 
 class Book(models.Model):
     title =  models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    author = models.CharField(null = True ,max_length=100)
+    is_bestselling=models.BooleanField(default= False)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
